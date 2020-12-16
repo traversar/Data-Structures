36a33ef7a212e1e7a07f04e94db4df956ef59f31
Andrew <travers.drew@gmail.com>
2020-12-15 23:25:28 -0500

    Linked List methods

--- linked_list.py
+++ linked_list.py
@@ -3,43 +3,64 @@ class LinkedList:
         self.head = head
         self.length = 0 if head == None else 1
 
-    def printList(self):
+    def print_list(self):
             current = self.head
             while current:
-                print (temp.data, " -> ", end = '')
-                current = current.next_node
+                print (current.data, " -> ", end = '')
+                current = current.next
             print("")
 
     def insert_head(self, data):
         new_node = Node(data)
-        new_node.next_node = self.head
+        new_node.next = self.head
         self.head = new_node
+        self.length += 1
+        return self.length
 
     # Insert at index
-    def insert(self, index, value):
+    def insert(self, index, data):
         current = self.head
         if index == 0:
-            return self.insert_at_head(value)
+            return self.insert_head(data)
         elif index == -1:
             index = self.length - 1
-        elif index >= self.length or index < -1:
+        elif index > self.length or index < -1:
             return False
         while index-1 > 0:
             current = current.next
+            index -= 1
         new_node = Node(data)
         new_node.next = current.next
         current.next = new_node
-        return self.length += 1
+        self.length += 1
+        return self.length
+
 
     # Remove specified node
     def remove(self, node):
-        pass
+        current = self.head
+        while current:
+            if current.next == node:
+                current.next, current.next.next = current.next.next, None
+                self.length -= 1
+                return self.length
+        return False
 
-    # Remove first node with specified value
-    def delete(self, value):
-        pass
+
+    # Remove first node with specified data value
+    def delete(self, data):
+        current = self.head
+        if current.data == data:
+            self.head, self.head.next = current.next, None
+        while current:
+            if current.next.data == data:
+                current.next, current.next.next = current.next.next, None
+                self.length -= 1
+                return self.length
+        return False
+
 
 class Node:
-    def __init__(self, data=None, next_node=None):
+    def __init__(self, data=None, next=None):
         self.data = data
-        self.next_node = next_node
+        self.next = next
